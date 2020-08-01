eabba5adafead5617e2f0db85b6b29ec7612b894
Nitin Ranganath <nitinranganath@gmail.com>
2020-08-01 13:04:36 +0530

    Initial routes setup

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const app = express();
 // Connect to MongoDB
 connectDB();
 
+// Initialize middleware
+app.use(express.json());
+
+// Define routes
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/users", require("./routes/api/users"));
+
 app.get("/", (req, res) => res.send("Hello World !"));
 
 const PORT = process.env.PORT || 5000;
