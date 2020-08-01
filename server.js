04806e0a7a48798bf810bc73b4bca1648486dde7
Nitin Ranganath <nitinranganath@gmail.com>
2020-08-01 12:38:18 +0530

    Initial backend server setup using express

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello World !"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
