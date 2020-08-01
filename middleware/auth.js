25d928d5e242db126d566730f1d6708a90f47839
Nitin Ranganath <nitinranganath@gmail.com>
2020-08-01 14:25:41 +0530

    Add JWT authentication middleware

---
+++ middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+module.exports = function (req, res, next) {
+  // Get token from header
+  const token = req.header("x-auth-token");
+
+  // Check if no token
+  if (!token) {
+    return res.send(401).json({ msg: "No token, authorization denied" });
+  }
+
+  // Verify token
+  try {
+    const decoded = jwt.verify(token, config.get("JWT_SECRET"));
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Invalid token" });
+  }
+};
--- routes/api/users.js
+++ routes/api/users.js
@@ -31,7 +31,7 @@ router.post(
     const { name, email, password } = req.body;
     try {
       // Check if the user already exists
-      const user = await User.findOne({ email });
+      let user = await User.findOne({ email });
       if (user) {
         return res.status(400).json({ msg: "User already exists" });
       }
