7e0b1388d36701e2c9f84af5d0ef6ef6b42f70ff
Nitin Ranganath <nitinranganath@gmail.com>
2020-08-02 14:21:45 +0530

    Get all profiles and get profiles by user ID API

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -104,4 +104,45 @@ router.get("/me", auth, async (req, res) => {
   }
 });
 
+// @route:    GET /api/profile
+// @desc:     Get all profiles
+// @access:   Public
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("User", ["name", "avatar"]);
+    res.json(profiles);
+  } catch {
+    console.error(err.message);
+    res
+      .status(500)
+      .send("There was an issue with the server. Try again later.");
+  }
+});
+
+// @route:    GET api/profile/user/:user_id
+// @desc:     Get profile by user ID
+// @access:   Public
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id,
+    }).populate("User", ["name", "avatar"]);
+
+    // If there's no profile
+    if (!profile) {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+
+    return res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === "ObjectId") {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+    res
+      .status(500)
+      .send("There was an issue with the server. Try again later.");
+  }
+});
+
 module.exports = router;
