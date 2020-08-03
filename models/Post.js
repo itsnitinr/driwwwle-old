ba663d058d48690d3e78bb528add41f980bd37cd
Nitin Ranganath <nitinranganath@gmail.com>
2020-08-03 14:29:13 +0530

    Minor change to post model

--- models/Post.js
+++ models/Post.js
@@ -30,8 +30,10 @@ const PostSchema = new mongoose.Schema({
   },
   likes: [
     {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
     },
   ],
   comments: [
