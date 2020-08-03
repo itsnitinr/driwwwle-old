c670bd4d79ce334129751a03230db7bb04a54ed4
Nitin Ranganath <nitinranganath@gmail.com>
2020-08-03 14:33:30 +0530

    Post like and unlike API

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -151,4 +151,69 @@ router.delete("/:post_id", auth, async (req, res) => {
   }
 });
 
+// @route:    PUT api/posts/like/:id
+// @desc:     Like a post
+// @access:   Private
+router.put("/like/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if post is already liked
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.status(400).json({ msg: "Post already liked" });
+    }
+
+    // Add to likes array
+    post.likes.unshift({ user: req.user.id });
+    await post.save();
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res
+      .status(500)
+      .send("There was an issue with the server. Try again later.");
+  }
+});
+
+// @route:    PUT api/posts/unlike/:id
+// @desc:     Unlike a post
+// @access:   Private
+router.put("/unlike/:id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if post exists
+    if (!post) {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+
+    // Check if post has been liked
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({ msg: "Post has not been liked yet" });
+    }
+
+    // Get remove index
+    const removeIndex = post.likes
+      .map((like) => like.user.toString())
+      .indexOf(req.user.id);
+    post.likes.splice(removeIndex, 1);
+    await post.save();
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == "ObjectId") {
+      return res.status(404).json({ msg: "Post not found" });
+    }
+    res
+      .status(500)
+      .send("There was an issue with the server. Try again later.");
+  }
+});
+
 module.exports = router;
