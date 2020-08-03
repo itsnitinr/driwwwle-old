3f83f23d5f185fade0f4abd4f92e64124f26d0cf
Nitin Ranganath <nitinranganath@gmail.com>
2020-08-03 16:24:50 +0530

    Add and delete comments API

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -68,7 +68,8 @@ router.get("/", auth, async (req, res) => {
   try {
     const posts = await Post.find()
       .sort({ date: -1 })
-      .populate("likes.user", ["name", "avatar"]);
+      .populate("likes.user", ["name", "avatar"])
+      .populate("comments.user", ["name", "avatar"]);
     res.json(posts);
   } catch (err) {
     console.error(err.message);
@@ -83,10 +84,9 @@ router.get("/", auth, async (req, res) => {
 // @access:   Private
 router.get("/:post_id", auth, async (req, res) => {
   try {
-    const post = await Post.findById(req.params.post_id).populate(
-      "likes.user",
-      ["name", "avatar"]
-    );
+    const post = await Post.findById(req.params.post_id)
+      .populate("likes.user", ["name", "avatar"])
+      .populate("comments.user", ["name", "avatar"]);
 
     // If there's no such post
     if (!post) {
@@ -112,7 +112,9 @@ router.get("/user/:user_id", auth, async (req, res) => {
   try {
     const posts = await Post.find({
       user: req.params.user_id,
-    }).populate("likes.user", ["name", "avatar"]);
+    })
+      .populate("likes.user", ["name", "avatar"])
+      .populate("comments.user", ["name", "avatar"]);
     if (!posts) {
       return res.status(400).json({ msg: "No posts found" });
     }
@@ -220,6 +222,70 @@ router.put("/unlike/:id", auth, async (req, res) => {
     res
       .status(500)
       .send("There was an issue with the server. Try again later.");
+  }
+});
+
+// @route:    POST api/post/comment/:post_id
+// @desc:     Post a comment
+// @access:   Private
+router.post(
+  "/comment/:post_id",
+  [auth, [check("text", "Comment text is required").not().isEmpty()]],
+  async (req, res) => {
+    // Check for validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const post = await Post.findById(req.params.post_id);
+      const newComment = {
+        user: req.user.id,
+        text: req.body.text,
+      };
+      post.comments.unshift(newComment);
+      await post.save();
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res
+        .status(500)
+        .send("There was an issue with the server. Try again later.");
+    }
+  }
+);
+
+// @route:    DELETE api/post/comment/:post_id/:comment_id
+// @desc:     Delete a comment
+// @access:   Private
+router.delete("/comment/:id/:comment_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if comment exists
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+    if (!comment) {
+      return res.status(404).json({ msg: "Comment does not exist" });
+    }
+
+    // Check if comment is posted by the same user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "Unauthorized" });
+    }
+
+    // Remove comment
+    const removeIndex = post.comments
+      .map((comment) => comment.user.toString())
+      .indexOf(req.user.id);
+    post.comments.splice(removeIndex, 1);
+    await post.save();
+    res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
   }
 });
 
