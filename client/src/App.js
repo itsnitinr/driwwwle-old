738cdb33555c046c9dc5bf70951205cacb1def25
Nitin Ranganath <nitinranganath@gmail.com>
2020-08-06 17:44:52 +0530

    Implement redux store

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
 
+import store from "./redux/store";
 import LandingPage from "./pages/landing-page/LandingPage.component";
 import SignupPage from "./pages/signup-page/SignupPage.component";
 import SigninPage from "./pages/signin-page/SigninPage.component";
@@ -9,13 +11,15 @@ import "./App.css";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={LandingPage} />
-        <Route exact path="/signup" component={SignupPage} />
-        <Route exact path="/signin" component={SigninPage} />
-      </Switch>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/signup" component={SignupPage} />
+          <Route exact path="/signin" component={SigninPage} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   );
 };
 
