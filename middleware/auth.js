296874f16d65b4197197e0d3181d4847bcc3ec11
Nitin Ranganath <nitinranganath@gmail.com>
2020-08-08 15:45:56 +0530

    Fix a minor bug in auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,7 +7,7 @@ module.exports = function (req, res, next) {
 
   // Check if no token
   if (!token) {
-    return res.send(401).json({ msg: "No token, authorization denied" });
+    return res.status(401).json({ msg: "No token, authorization denied" });
   }
 
   // Verify token
