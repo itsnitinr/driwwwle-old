1bf9af4f0a0f31f071614cd4e35454700673b6fe
Nitin Ranganath <nitinranganath@gmail.com>
2020-08-09 22:48:42 +0530

    Add link to go to landing page on auth panel component for desktop view

--- client/src/components/auth-left-pane/AuthLeftPane.component.jsx
+++ client/src/components/auth-left-pane/AuthLeftPane.component.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./AuthLeftPane.styles.css";
 
 const AuthLeftPane = ({ children }) => {
   return (
     <div className="column is-one-third" id="auth-left-panel">
       <div className="text-container">
-        <h1>driwwwle</h1>
+        <Link to="/" className="logo">
+          driwwwle
+        </Link>
         <p>{children}</p>
       </div>
     </div>
