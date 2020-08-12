886e39e69dc78ff180fe729f517d6a5e1fa7df46
Nitin Ranganath <nitinranganath@gmail.com>
2020-08-12 22:32:46 +0530

    Better styling for follow list component

--- client/src/components/follow-list/FollowList.component.jsx
+++ client/src/components/follow-list/FollowList.component.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 
-const FollowList = ({ list, which }) => {
+import "./FollowList.styles.css";
+
+const FollowList = ({ list, which, history }) => {
   return (
     <>
       {list.length === 0 ? (
-        <h1>No {`${which}`}</h1>
+        <h1 className="title">No {`${which}`}</h1>
       ) : (
-        list.map((follow) => <h1 key={follow._id}>{follow.user.name}</h1>)
+        <div className="follow-list columns is-multiline">
+          {list.map((follow) => (
+            <div
+              key={follow.user._id}
+              className="follow-list-item column is-2 is-4-mobile"
+            >
+              <img src={follow.user.avatar} alt="Follow avatar" />
+              <h1 className="has-text-weight-bold">{follow.user.name}</h1>
+            </div>
+          ))}
+        </div>
       )}
     </>
   );
