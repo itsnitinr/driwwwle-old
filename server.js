ced7d0d58aa300e860f0866cf32196264fed7566
Nitin Ranganath <nitinranganath@gmail.com>
2020-08-13 17:15:09 +0530

    Getting ready for initial Heroku deploy

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const connectDB = require("./config/db");
 const app = express();
 
@@ -14,7 +15,13 @@ app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/users", require("./routes/api/users"));
 
-app.get("/", (req, res) => res.send("Hello World !"));
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
