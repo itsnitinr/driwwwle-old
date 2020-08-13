da093fe479d489bb4b3857d4d578a2582fb3dd5a
Nitin Ranganath <nitinranganath@gmail.com>
2020-08-13 16:28:23 +0530

    Add coming soon page

---
+++ client/src/pages/coming-soon/ComingSoonPage.component.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import Navbar from "../../components/navbar/Navbar.component";
+import Footer from "../../components/footer/Footer.component";
+import { ReactComponent as ComingSoonImage } from "../../assets/images/coming-soon.svg";
+
+import "./ComingSoonPage.styles.css";
+
+const ComingSoonPage = () => {
+  return (
+    <>
+      <Navbar />
+      <div id="coming-soon" className="container px-5">
+        <div>
+          <ComingSoonImage id="coming-soon-image" />
+        </div>
+        <h1 className="mt-5">
+          Hang in there ! We're working on some excited features.
+        </h1>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default ComingSoonPage;
