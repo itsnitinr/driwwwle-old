63fd2ffe12744ce1216ec4707af80c0eb9467c76
Nitin Ranganath <nitinranganath@gmail.com>
2020-08-15 22:33:52 +0530

    Fix typo in coming soon page

--- client/src/pages/coming-soon/ComingSoonPage.component.jsx
+++ client/src/pages/coming-soon/ComingSoonPage.component.jsx
@@ -15,7 +15,7 @@ const ComingSoonPage = () => {
           <ComingSoonImage id="coming-soon-image" />
         </div>
         <h1 className="mt-5">
-          Hang in there ! We're working on some excited features.
+          Hang in there ! We're working on some exciting features.
         </h1>
       </div>
       <Footer />
